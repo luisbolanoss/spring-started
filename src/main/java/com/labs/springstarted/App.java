b6664cdee4a102b105a356f34acf16b3d9b7c5dc
Luis Alberto Bolaños Salazar <lbolanoss@psl.com.co>
2020-02-23 18:41:59 -0500

    Create Maven project

---
+++ src/main/java/com/labs/springstarted/App.java
@@ -0,0 +1,13 @@
+package com.labs.springstarted;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
